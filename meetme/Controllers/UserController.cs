b1126bec72c9cfe2f3c5af26bdb2d0e4840eb866
mhyassin <m.hisham92@hotmail.com>
2014-02-08 15:50:18 +0200

    return null when user is null

--- meetme/Controllers/UserController.cs
+++ meetme/Controllers/UserController.cs
@@ -12,36 +12,35 @@ namespace meetme.Controllers
     public class UserController : ApiController
     {
         private MeetmeContext dbModel = new MeetmeContext();
-        
-        // GET api/values
-        public IEnumerable<string> Get()
+
+        // GET api/user/5
+        public string Get(Guid id)
         {
-            return new string[] { "value1", "value2" };
+            User user = dbModel.Users.FirstOrDefault(w => w.id.Equals(id));
+            if (user != null)
+            {
+                return Newtonsoft.Json.JsonConvert.SerializeObject(user);
+            }
+            return "null";
         }
 
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
+        // POST api/user
         public string Post([FromBody]string email, [FromBody]string token)
         {
             List<User> user = dbModel.Users.Where(w => w.email.Equals(email) && w.password.Equals(token)).ToList();
             if (user.Count > 0)
             {
-                return "true";
+                return user[0].id.ToString();
             }
             return "false";
         }
 
-        // PUT api/values/5
+        // PUT api/user/5
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/user/5
         public void Delete(int id)
         {
         }
