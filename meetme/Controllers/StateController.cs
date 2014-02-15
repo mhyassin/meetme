5a1c86cb8ef59d4e50cbc581a5ea2122b811e135
mhyassin <m.hisham92@hotmail.com>
2014-02-15 05:43:22 +0200

    getfeed

---
+++ meetme/Controllers/StateController.cs
@@ -0,0 +1,27 @@
+﻿using meetme.Entities;
+using Meetme.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace meetme.Controllers
+{
+    public class StateController : ApiController
+    {
+        private MeetmeContext dbModel = new MeetmeContext();
+
+        [HttpGet]
+        public List<State> GetFeed(Guid id)
+        {
+            User user = dbModel.Users.FirstOrDefault(w => w.id.Equals(id));
+            List<State> feed = dbModel.States.Where(w=> user.following.Contains(w.UserId.id)).ToList();
+            if (feed != null)
+            {
+                return feed;
+            }
+            return null;
+        }
+    }
+}
--- meetme/Entities/State.cs
+++ meetme/Entities/State.cs
@@ -9,11 +9,11 @@ namespace meetme.Entities
     public class State
     {
         public Guid id { set; get; }
-        public Guid UserId  { set; get; }
+        public User UserId { set; get; }
         public bool isRoom { set; get; }
         public bool isOfficial { set; get; }
-        public Guid RoomId { set; get; }
-        public Guid OtherUserId { set; get; }
+        public MeetingRoom RoomId { set; get; }
+        public User OtherUserId { set; get; }
         public DateTime startDate { set; get; }
         public double duration { set; get; }
     }
